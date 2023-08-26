c465d41465e29339c20deac9e2043b5f4c1b3a7b
torben.eims <torben.eims@stud.uni-hannover.de>
2023-08-27 00:51:43 +0200

    update scripts

--- scripts/run_hyfd.sh
+++ scripts/run_hyfd.sh
@@ -1,1 +1,1 @@
-java -Xms50g -Xmx50G -cp algorithms/metanome-cli.jar:algorithms/hyfd.jar de.metanome.cli.App --algorithm de.metanome.algorithms.hyfd.HyFD --file-key INPUT_GENERATOR --files $1
+java -Xms256g -Xmx256G -cp algorithms/metanome-cli.jar:algorithms/hyfd.jar de.metanome.cli.App --algorithm de.metanome.algorithms.hyfd.HyFD --file-key INPUT_GENERATOR --files $1
